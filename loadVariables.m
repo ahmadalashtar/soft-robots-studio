9ad807b9d1218429bcd2efd4a9d8cf472fae83e0
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-10-31 14:10:36 +0300

    Refactor

--- loadVariables.m
+++ loadVariables.m
@@ -1,14 +1,5 @@
 function loadVariables(opVar)
-    global op;    
-    op.home_base = opVar.home_base
-    op.targets = opVar.targets;
-    op.obstacles = opVar.obstacles;
-    op.n_nodes = opVar.n_nodes;
-    op.angle_domain = opVar.angle_domain;
-    op.length_domain = opVar.length_domain;
-        
-    op.first_angle.is_fixed = opVar.first_angle.is_fixed;
-    op.first_angle.angle = opVar.first_angle.angle;
-   
+   global op;    
+   op = opVar
 end
 
