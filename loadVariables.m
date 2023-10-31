c4d0e30d5326419993f06bf8664891dd4d873d53
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-10-31 12:10:43 +0300

    Finalize loading variables

--- loadVariables.m
+++ loadVariables.m
@@ -1,4 +1,13 @@
-function loadVariables(variables)
-    variables
+function loadVariables(opVar)
+    global op;    
+    op.targets = opVar.targets;
+    op.obstacles = opVar.obstacles;
+    op.n_nodes = opVar.n_nodes;
+    op.angle_domain = opVar.angle_domain;
+    op.length_domain = opVar.length_domain;
+        
+    op.first_angle.is_fixed = opVar.first_angle.is_fixed;
+    op.first_angle.angle = opVar.first_angle.angle;
+   
 end
 
