2adb6702897cff04b2dffe0b33a99e4cfbd26cce
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-11-15 15:09:35 +0300

    Add end_points to loadVariables.m

--- loadVariables.m
+++ loadVariables.m
@@ -1,5 +1,10 @@
-function loadVariables(opVar)
-   global op;    
-   op = opVar
+function loadVariables(opVar,end_points)
+   global op;
+   if end_points == 0
+       op = opVar;
+   else
+       op.end_points = end_points;
+   end
+   
 end
 
