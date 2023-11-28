d776ae114493944434e39a17d7422f5cf651ea31
Ahmad Alashtar <ahmadalashtar@gmail.com>
2023-11-28 16:11:45 +0300

    Script to load runItGeneric variables

---
+++ loadRunItGeneric.m
@@ -0,0 +1,11 @@
+function loadRunItGeneric(algoVar, gasVar, bbbcsVar)
+   global algorithm;
+   global gas;
+   global bbbcs;
+   
+   algorithm = algoVar;
+   gas = gasVar;
+   bbbcs = bbbcsVar;
+
+end
+
