154543229fdf1695b85febbef739077d8df29401
Ozi6 <magnusterrarian@gmail.com>
2024-06-24 10:27:34 +0300

    Segment Bug Fix (Partially) Complete
    +Properly draws the segment lines now on MP.
    -New bug that gives error.

---
+++ utils/draw_segments_MP.m
@@ -0,0 +1,27 @@
+function draw_segments_MP(app,axes,var)
+    try
+        points = retrieveOrientationSegmentEndPointsMP(false,var);
+        loadOP(app.op, points);
+
+        % one target => points = [ x ; y]
+        % two targets => points = [x y ; x y x y]
+        if length(app.TargetsNode.Children)==1
+
+            child = app.TargetsNode.Children(1);
+            plot(axes,[points(1) child.NodeData.x],[points(2) child.NodeData.y], 'Color','blue','LineStyle','--')
+            plot(axes, points(1),points(2),'ko','markerfacecolor','k')
+
+        else
+            for i = 1:length(points)
+                point = points(i,:);
+                child = app.TargetsNode.Children(i);
+                plot(axes,[point(1) child.NodeData.x],[point(2) child.NodeData.y], 'Color','blue','LineStyle','--')
+                plot(axes, point(1),point(2),'ko','markerfacecolor','k')
+            end
+        end
+
+    catch ME
+        % uialert(app.UIFigure,getReport(ME),'Error')
+        uialert(app.UIFigure,ME.message,'Error')
+    end
+end
