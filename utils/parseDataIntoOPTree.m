76de7664b8de28d18b5ff8937d2335392403d646
Badallocoredumped <emirozen57@hotmail.com>
2024-07-08 16:05:38 +0300

    File-Import-Bug
    -Now the app does not give an error when loading a file with no generations.

--- utils/parseDataIntoOPTree.m
+++ utils/parseDataIntoOPTree.m
@@ -1,11 +1,7 @@
 function parseDataIntoOPTree(app)
-    children = app.loadedData.saveVar.OPTreeChildren;
-
-    if numel(children) > 0
-        for i = 1 : numel(children)
-            uitreenode(app.OPTree,"Text",children(i).Text,"NodeData",children(i).NodeData);
-        end
-        app.OPTree.SelectedNodes = app.OPTree.Children(end);
+    for i = 1 : numel(children)
+        uitreenode(app.OPTree,"Text",children(i).Text,"NodeData",children(i).NodeData);
     end
+    app.OPTree.SelectedNodes = app.OPTree.Children(end);
     %OPTreeSelection(app, 0) Another way to call private function
 end
