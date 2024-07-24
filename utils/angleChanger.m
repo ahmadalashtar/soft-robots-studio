4c09d165f5ad68767ac3cb63cbca586a3b1345ae
Ozi6 <magnusterrarian@gmail.com>
2024-07-24 12:06:42 +0300

    Revert "Multiple Pop-Up Fix"
    This reverts commit ffc74a75126a533c8f6effa39347578c4f2fd605.

--- utils/angleChanger.m
+++ utils/angleChanger.m
@@ -1,4 +1,21 @@
 function angleChanger(src, event, app)
+    if numel(app.OPTree.Children)>1 && app.Eraser.State == "off"
+        result = confirmAction(app,'Editing',"Loss in Optimizer's Output Will Occur");
+        if result  == "OK"
+            delete(app.OPTree.Children);
+            app.genHold = -10;
+            app.holdNode = -10;
+            buttons = findall(app.TabGroup2, 'Tag', 'robotPara');
+            for k = 1:length(buttons)
+                buttons(k).Enable = 'on';
+            end
+            app.Continuar.Enable = 'off';
+    
+        else
+            return;
+        end
+
+    end
     val = event.Value;
     app.AngledegEditField.Value(1) = val;
     
