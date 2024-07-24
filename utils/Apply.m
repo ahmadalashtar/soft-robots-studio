b6dd4329df9fe9015779c869971b7ce899c940e7
Ozi6 <magnusterrarian@gmail.com>
2024-07-24 22:00:04 +0300

    Apply Function Made Faster
    -Pretty small changes, mostly just the switch-case and moving of where values are changed but due to the amount of times Apply is called, this way it is n times faster, n being total amount of elements in the UIAxes.

--- utils/Apply.m
+++ utils/Apply.m
@@ -1,43 +1,69 @@
 function Apply(app)
     selectedNode = app.Tree.SelectedNodes;
     child = selectedNode.NodeData.child;
-    if selectedNode.Parent == app.TargetsNode
-        t = child.DataTipTemplate.DataTipRows(end);
-    end
-    delete(child);
+
+    %These operations are common for all of the possible Apply-able UI parts(obstacle, target base),
+    %so they're made at the top.
+
     x = app.XcoordinateEditField.Value;
     y = app.YcoordinateEditField.Value;
-    selectedNode.NodeData.x=x;
-    selectedNode.NodeData.y=y;
-    if selectedNode.Parent == app.TargetsNode
-        % L = app.LengthEditField.Value;
-        angle = app.AngledegEditField.Value(1) ;
-        scaleHold = app.scalerOP;
-        selectedNode.NodeData.length = 2;
-        selectedNode.NodeData.angle = angle;
-        selectedNode.NodeData.currentScale = scaleHold;
-        selectedNode.Text = "x: " + string(round(x,2)) + ", y: " + string(round(y,2)) + ", angle: " + string(round(angle,2))  + ", current scale: " + string(round(scaleHold,2));
-        ps = draw_target(app,x,y,angle,app.UIAxes1, scaleHold);
-        ps.DataTipTemplate.DataTipRows(end) = t;
-        
-    elseif selectedNode.Parent == app.ObstaclesNode
-        radius = app.LengthEditField.Value;
-        selectedNode.NodeData.radius = radius;
-        selectedNode.Text = "x: " + string(round(x,2)) + ", y: " + string(round(y,2)) + ", radius: " + string(round(radius,2));
-        ps = draw_obstacle(app,x,y,radius,app.UIAxes1);
-    elseif selectedNode.Parent == app.BaseNode
-        angle = app.AngledegEditField.Value(1) ;
-        selectedNode.NodeData.length = 1;
-        selectedNode.NodeData.angle = angle;
-        selectedNode.Text = "x: " + string(round(x,2)) + ", y: " + string(round(y,2)) + ", angle: " + string(round(angle,2));
-        ps = draw_base(app,x,y,angle,app.UIAxes1);
+
+    selectedNode.NodeData.x = x;
+    selectedNode.NodeData.y = y;
+
+    %Switch-cases for all the possible node types.
+    switch selectedNode.Parent
+        case app.TargetsNode
+
+            t = child.DataTipTemplate.DataTipRows(end);
+            angle = app.AngledegEditField.Value(1);
+            scaleHold = app.scalerOP;
+
+            selectedNode.NodeData.length = 2;
+            selectedNode.NodeData.angle = angle;
+            selectedNode.NodeData.currentScale = scaleHold;
+            selectedNode.Text = formatText(x, y, angle, scaleHold);
+
+            ps = draw_target(app, x, y, angle, app.UIAxes1, scaleHold);
+            ps.DataTipTemplate.DataTipRows(end) = t;
+            
+        case app.ObstaclesNode
+            radius = app.LengthEditField.Value;
+
+            selectedNode.NodeData.radius = radius;
+            selectedNode.Text = formatText(x, y, radius);
+
+            ps = draw_obstacle(app, x, y, radius, app.UIAxes1);
+            
+        case app.BaseNode
+            angle = app.AngledegEditField.Value(1);
+            
+            selectedNode.NodeData.length = 1;
+            selectedNode.NodeData.angle = angle;
+            selectedNode.Text = formatText(x, y, angle);
+
+            ps = draw_base(app, x, y, angle, app.UIAxes1);
+
     end
     
+    %Common operations after getting the node drawn back on the Axes
+
+    delete(child);
+
     ps.UserData = selectedNode;
-    %ps.FaceAlpha = 0.1;
     selectedNode.NodeData.child = ps;
-    ps.ButtonDownFcn = @(src,event)ps_mouse_click(app,src);
-    ps_mouse_click(app,ps);
+    ps.ButtonDownFcn = @(src, event)ps_mouse_click(app, src);
+    ps_mouse_click(app, ps);
 
+end
 
+function text = formatText(x, y, variable)
+    %Formatting the text based on the number of input arguments
+    text = "x: " + string(round(x, 2)) + ", y: " + string(round(y, 2));
+    if nargin > 2
+        text = text + ", angle: " + string(round(variable{1}, 2));
+        if nargin > 3
+            text = text + ", current scale: " + string(round(variable{2}, 2));
+        end
+    end
 end
