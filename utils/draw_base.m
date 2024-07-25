fdbbc7f31b68cddb7af37970045fba4f147a7110
Ozi6 <magnusterrarian@gmail.com>
2024-07-25 11:07:46 +0300

    Base Coordinates Bug Fixed
    -Now the bases coordinates can be changed.

--- utils/draw_base.m
+++ utils/draw_base.m
@@ -29,16 +29,27 @@ function ps = draw_base(app,x,y,angle,axes)
     y_upper_right=center2+T(2,3);
     y_upper_left=center2+T(2,4);
     
-    x_coor=[x_upper_left x_lower_left x_lower_right x_upper_right ];
-    y_coor=[y_upper_left y_lower_left y_lower_right y_upper_right ];
+    % Define coordinates
+    x_coor = [x_upper_left, x_lower_left, x_lower_right, x_upper_right];
+    y_coor = [y_upper_left, y_lower_left, y_lower_right, y_upper_right];
+    
+    % Determine scale based on the selected tab
     if app.selectedTab == app.OptimizerTab
         scale = sqrt(6) * app.scalerOP;
     else
         scale = sqrt(6) * app.scalerMP;
     end
-    x_coor = x_coor * scale;
-    y_coor = y_coor * scale;
-    shape = polyshape(x_coor,y_coor);
+    
+    % Calculate the centroid of the shape
+    centroid_x = mean(x_coor);
+    centroid_y = mean(y_coor);
+    
+    % Scale the coordinates relative to the centroid
+    x_coor_scaled = centroid_x + (x_coor - centroid_x) * scale;
+    y_coor_scaled = centroid_y + (y_coor - centroid_y) * scale;
+    
+    % Create the scaled shape
+    shape = polyshape(x_coor_scaled, y_coor_scaled);
     ps = plot(axes,shape,'LineWidth',1,"FaceColor",'#001aff', 'FaceAlpha',1);
     
 end
