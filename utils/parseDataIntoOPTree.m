98fcc751102e7370a34faa178a4aef7faf079046
Ozi6 <magnusterrarian@gmail.com>
2024-08-05 10:13:49 +0300

    Hold Push

--- utils/parseDataIntoOPTree.m
+++ utils/parseDataIntoOPTree.m
@@ -2,7 +2,7 @@ function parseDataIntoOPTree(app)
     children = app.loadedData.saveVar.OPTreeChildren;
     if numel(children) > 0
         for i = 1 : numel(children)
-            uitreenode(app.OPTree,"Text",children(i).Text,"NodeData",children(i).NodeData);
+            uitreenode(app.OPTree,"Text",children(i).Text,"NodeData",children(i).NodeData,"Icon",children(i).Icon);
         end
         app.OPTree.SelectedNodes = app.OPTree.Children(end);
     end
